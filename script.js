981d31e4590eaad173296768366be2b08272d74d
myamusashi <jinxprogilang666@gmail.com>
2024-01-09 10:54:20 +0700

    tehe

--- script.js
+++ script.js
@@ -63,11 +63,11 @@ function updateTime() {
 setInterval(updateTime, 10);
 
 // Add an event listener for when the user leaves the page
-window.addEventListener('beforeunload', function(e) {
-    // Cancel the default event
-    e.preventDefault();
+// window.addEventListener('beforeunload', function(e) {
+//     // Cancel the default event
+//     e.preventDefault();
 
-    // Add your animation or transition logic here
-    // For example, fading out the content
-    document.body.style.opacity = '0'; // Change the opacity to create a fade-out effect
-});
+//     // Add your animation or transition logic here
+//     // For example, fading out the content
+//     document.body.style.opacity = '0'; // Change the opacity to create a fade-out effect
+// });
