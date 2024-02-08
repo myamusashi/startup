3d9ed2fa9db0d00dae0d2eeef6f84ee381d8b19b
myamusashi <jinxprogilang666@gmail.com>
2024-02-09 06:55:44 +0700

    remove js script

--- cat.js
+++
@@ -1,6 +0,0 @@
-fetch("https://api.thecatapi.com/v1/images/search")
-  .then((response) => response.json())
-  .then((data) => {
-    const imageUrl = data[0].url;
-    console.log(`${imageUrl}`);
-  });
